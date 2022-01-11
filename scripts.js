87931a664137a1e26114afefdc15589e00aaead2
jerushaotieno <jerushaotienocoding@gmail.com>
2022-01-11 12:17:32 +0300

    add JS code for receiving emails from users

--- scripts.js
+++ scripts.js
@@ -31,5 +31,14 @@ $(document).ready(function() {
         $(this).hide();
         $('#product_management').toggle();
     });
+
+    $("form#contact_form").submit(function(event) {
+        event.preventDefault();
+        var name = $("#contact_name").val();
+        alert("Hi " + name + " Thank you for getting in touch. Your email is well received.");
+        document.getElementById("contact_form").reset();
+    })
 });
+
+
 
