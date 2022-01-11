68ab5a62f22e7cce1bef163838b6f39202e2f69f
jerushaotieno <jerushaotienocoding@gmail.com>
2022-01-11 21:07:33 +0300

    add JS code for toggling "what we do" section when clicked

--- scripts.js
+++ scripts.js
@@ -12,25 +12,23 @@ $(document).ready(function() {
         $('.toggle_row >img').toggleClass('d-none');
     });
 
-//     $('#development_icon').click(function() {
-//         $(this).hide();
-//         $('.development_content').toggle();
-//     });
+    // $('.toggle_row >img').click(function() {
+    //     $(this).toggleClass('d-none');
+    //     $('.design_content').toggleClass('d-none');
+    // });
 
-//     $('.development_content').click(function() {
-//         $(this).hide();
-//         $('#development_icon').toggle();
-//     });
+    // $('.design_content').click(function() {
+    //     $(this).toggleClass('d-none');
+    //     $('.toggle_row >img').toggleClass('d-none');
+    // });
 
-//     $('#product_management_icon').click(function() {
-//         $(this).hide();
-//         $('.product_management_content').toggle();
-//     });
-
-//     $('.product_management_content').click(function() {
-//         $(this).hide();
-//         $('#product_management').toggle();
-//     });
+    $('.portfolio_row_item').each(function(){
+        $(this).hover(function mouseEnter(){
+            $('.portfolio_row_item >p').removeClass('d-none')
+        },function mouseLeave(){
+            $('.portfolio_row_item >p').addClass('d-none')
+        })
+    })
 
 //JS code for contacting us via email
     $("#contact_form").submit(function(event) {
