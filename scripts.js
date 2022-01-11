5f74132a8e645f6bd668786806235219d5249853
jerushaotieno <jerushaotienocoding@gmail.com>
2022-01-11 21:48:28 +0300

    complete JS code for toggling the "what we do" section

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-    //toggle for the "What We Do" section
+    // JS code to add toggle effect for the "What We Do" section
 
     $('.toggle_row >img').click(function() {
         $(this).toggleClass('d-none');
@@ -12,15 +12,27 @@ $(document).ready(function() {
         $('.toggle_row >img').toggleClass('d-none');
     });
 
-    // $('.toggle_row >img').click(function() {
-    //     $(this).toggleClass('d-none');
-    //     $('.design_content').toggleClass('d-none');
-    // });
+    $('.toggle_row >img').click(function() {
+        $(this).toggleClass('d-none');
+        $('.development_content').toggleClass('d-none');
+    });
 
-    // $('.design_content').click(function() {
-    //     $(this).toggleClass('d-none');
-    //     $('.toggle_row >img').toggleClass('d-none');
-    // });
+    $('.development_content').click(function() {
+        $(this).toggleClass('d-none');
+        $('.toggle_row >img').toggleClass('d-none');
+    });
+
+    $('.toggle_row >img').click(function() {
+        $(this).toggleClass('d-none');
+        $('.product_management_content').toggleClass('d-none');
+    });
+
+    $('.product_management_content').click(function() {
+        $(this).toggleClass('d-none');
+        $('.toggle_row >img').toggleClass('d-none');
+    });
+
+//JS code to create overlay effect on the "portfolio section"
 
     $('.portfolio_row_item').each(function(){
         $(this).hover(function mouseEnter(){
@@ -28,9 +40,10 @@ $(document).ready(function() {
         },function mouseLeave(){
             $('.portfolio_row_item >p').addClass('d-none')
         })
-    })
+    });
 
 //JS code for contacting us via email
+
     $("#contact_form").submit(function(event) {
         event.preventDefault();
         var name = $("#contact_name").val();
